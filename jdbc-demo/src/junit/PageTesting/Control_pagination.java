5a00c39652ff27fd09809f18707878014b578e8c
6wensitan <pch124678@163.com>
2023-09-15 15:08:46 +0800

    杰普的代码和算法

--- jdbc-demo/src/junit/PageTesting/Control_pagination.java
+++ jdbc-demo/src/junit/PageTesting/Control_pagination.java
@@ -7,6 +7,14 @@ package src.junit.PageTesting;
  */
 public class Control_pagination {
     public static void main(String[] args) {
-
+        System.out.println("hdduysbsd ");
+        int a= 12;
+        pageUtils(a);
+    }
+    public static void pageUtils(int a) {
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(i+ a);
+        }
     }
 }
